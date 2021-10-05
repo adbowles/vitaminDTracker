5c264f7300e14fbfa7debd6cfd7c0354c81f7908
adbowles <89156422+adbowles@users.noreply.github.com>
2021-10-04 20:34:54 -0500

    day 1 presentations

---
+++ screens/TabTwoScreen.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import { StyleSheet, TouchableWithoutFeedback, Keyboard, TextInput } from 'react-native';
+import {Pressable} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+
+export default function TabTwoScreen() {
+  const [inches, setInches] = React.useState(null);
+  const [feet, setFeet] = React.useState(null);
+  const [weight, setWeight] = React.useState(null);
+  const [BMI, setBMI] = React.useState(null);
+
+  const inchChanger = (inches) => {
+    if (inches > 11) inches = '11';
+    setInches(inches);
+  }
+  const feetChanger = (feet) => {
+    if (feet > 7) feet = '7';
+    setFeet(feet);
+  }
+  const weightChanger = (weight) => {
+    setWeight(weight);
+  }
+  const calcBMI = () => {
+    // kg / m^2
+    // ( lbs / in^2 )* 703
+    if (inches > 0 || feet > 0) {
+      let totalInches = parseInt(feet)*12 + parseInt(inches);
+      let thisBMI = (parseInt(weight) * 703) / (totalInches*totalInches);
+      setBMI(thisBMI);
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();calcBMI();}} accessible={false}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Settings</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Height:</Text>
+          <TextInput
+            onChangeText={feetChanger}
+            value={feet}
+            keyboardType="numeric"
+            style={styles.inputSmall}
+            maxLength={1}
+          />
+          <Text style={styles.inputLabel}>ft</Text>
+          <TextInput
+            onChangeText={inchChanger}
+            value={inches}
+            keyboardType="numeric"
+            style={styles.inputSmall}
+            maxLength={2}
+          />
+          <Text style={styles.inputLabel}>in</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Weight:</Text>
+          <TextInput
+            onChangeText={weightChanger}
+            value={weight}
+            keyboardType="numeric"
+            style={styles.input}
+            maxLength={3}
+          />
+          <Text style={styles.inputLabel}>lbs</Text>
+        </View>
+        <Text style={styles.BMILabel}>BMI: {BMI ? BMI.toFixed(2) : 0}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: "center",
+    position: "absolute",
+    top: '5%',
+  },
+  inputContainer: {
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems:'center',
+    alignSelf:"flex-start",
+    marginLeft:'10%',
+  },
+  inputLabel: {
+    fontSize:30,
+    // fontWeight:'bold'
+  },
+  input: {
+    fontSize: 30,
+    backgroundColor:'white',
+    margin:10,
+    width:'30%',
+    textAlign:'center',
+    padding:5,
+  },
+  inputSmall: {
+    fontSize: 30,
+    backgroundColor:'white',
+    margin:10,
+    width:60,
+    textAlign:'center',
+    padding:5,
+  },
+  BMILabel: {
+    marginTop:30,
+    fontSize:20
+  }
+});
