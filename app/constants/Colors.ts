cc32f4ae997cd0c8aa4e0d59a1279fafb6e99f48
adbowles <adbowles@crimson.ua.edu>
2021-12-07 00:52:27 -0600

    Revert "Revert "Merge branch 'BlueTooth' into adbowles-patch-1""
    This reverts commit eca0d81396060901db69ff239b0910b4b4921882.

--- app/constants/Colors.ts
+++ app/constants/Colors.ts
@@ -4,14 +4,14 @@ const tintColorDark = '#fff';
 export default {
   light: {
     text: '#000',
-    background: '#36A900',
+    background: '#48AF00',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#fff',
-    background: '#fffff',
+    background: '#1b1f38',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
